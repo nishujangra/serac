3ac717e3f392a4315e8818adff7d35209df06d1f
nishujangra <ndjangra1027@gmail.com>
2025-07-23 15:00:32 +0530

    feat(ui): login page

--- src/routes/auth_routes.rs
+++ src/routes/auth_routes.rs
@@ -2,16 +2,17 @@
 use rocket::serde::json::Json;
 use crate::models::user::UserLogin;
 
+use rocket_dyn_templates::{Template, context};
+
 #[get("/login")]
-pub fn login_page() -> String {
-    format!("This is the login page")
+pub fn login_page() -> Template {
+    Template::render("auth/login", context!{})
 }
 
 #[post("/login", format= "json", data = "<user>")]
-pub fn login(user: Json<UserLogin>) -> String {
+pub fn login(user: Json<UserLogin>, db: &State<PgPool>) -> Result<Redirect, Status> {
     format!("print test user {:?}", user.0)
 }
-
 
 // #[get("/register")]
 // pub fn register_page()
